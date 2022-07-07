d290ffd59572b8f6f05dfa01adb3523d738ade71
iamolami <musediqolamilekan567@gmail.com>
2022-07-07 05:20:45 +0100

    Sort data by name, search and query data by name(auto complete),latitude, and longitude

--- controllers/suggestionsCtrl.js
+++ controllers/suggestionsCtrl.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const assert = require('assert')
-const { StatusCodes } = require('http-status-codes')
+const {StatusCodes} = require('http-status-codes')
 
 const suggestionCtrl = {
 
@@ -10,13 +10,17 @@ const suggestionCtrl = {
             let results = [];
             mongoose.connect(process.env.MONGO_URI, (err, db) => {
                 assert.equal(null, err);
-                const cursor = db.collection('sunsnifferCollection').find();
+                const cursor = db
+                    .collection('sunsnifferCollection')
+                    .find();
                 cursor.forEach((doc, err) => {
                     assert.equal(null, err);
                     results.push(doc);
                 }, () => {
                     db.close();
-                    res.status(StatusCodes.OK).json({ results: results })
+                    res
+                        .status(StatusCodes.OK)
+                        .json({results: results})
                 });
             })
 
@@ -31,15 +35,30 @@ const suggestionCtrl = {
 
         try {
             let results = [];
-            mongoose.connect(process.env.MONGO_URI, (err, db) => {
+            const q = req.query.q;
+            const latitude = req.query.latitude;
+            const longitude = req.query.longitude;
+
+            mongoose.connect(process.env.MONGO_URI, async (err, db) => {
                 assert.equal(null, err);
-                const cursor = db.collection('sunsnifferCollection').find();
+                const cursor = await db
+                    .collection('sunsnifferCollection')
+                    .find({
+                        name: {
+                            $regex: new RegExp('^' + q + '.*', 'i')
+                        },
+                        lat: latitude,
+                        long: longitude
+                    })
+                    .sort({name: 1});
                 cursor.forEach((doc, err) => {
                     assert.equal(null, err);
                     results.push(doc);
                 }, () => {
                     db.close();
-                    res.status(StatusCodes.OK).json({ suggestions: results })
+                    res
+                        .status(StatusCodes.OK)
+                        .json({suggestions: results})
                 });
             })
 
@@ -48,7 +67,7 @@ const suggestionCtrl = {
                 .status(StatusCodes.INTERNAL_SERVER_ERROR)
                 .json({message: err.message, success: false});
         }
-    },
+    }
 }
 
 module.exports = suggestionCtrl
--- routes/suggestionsRoute.js
+++ routes/suggestionsRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const suggestionsCtrl = require('../controllers/suggestionsCtrl');
 
 router.route('/get-data').get(suggestionsCtrl.getData);
+router.route('/suggestions').get(suggestionsCtrl.suggestions);
 
 module.exports = router;
